4612d1bd9a56f0a92f3cd03b2fdc56c8b6054c25
Lesly Anda <lesly.anda@cimat.mx>
2025-02-10 22:47:16 -0600

    Update 2_Problem27.py

--- Python Code/2_Problem27.py
+++ Python Code/2_Problem27.py
@@ -1,4 +1,5 @@
 # 27. Remove Element
+# https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
